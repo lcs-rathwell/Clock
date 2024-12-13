2791da97bd452a858e540c713eb8e81ed3fe7660
Jack Rathwell <jrathwell@lcs.on.ca>
2024-12-13 14:00:26 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Jack William Rathwell on 2024-12-13.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

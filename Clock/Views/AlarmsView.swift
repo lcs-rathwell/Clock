1eff614021f40805e507a27a59ae2a3aaec660db
Jack Rathwell <jrathwell@lcs.on.ca>
2024-12-18 11:32:34 -0500

    Fixed interface, made it push left

--- Clock/Views/AlarmsView.swift
+++ Clock/Views/AlarmsView.swift
@@ -11,29 +11,38 @@ struct AlarmsView: View {
     var body: some View {
         NavigationStack{
             VStack{
-                HStack {
-                    Image(systemName: "bed.double.fill")
-                        .foregroundColor(.white)
-                        .font(.title2)
-                    Text("Sleep | Wake Up")
-                        .foregroundColor(.white)
-                        .font(.headline)
-                    
+                ZStack {
+                    HStack {
+                        Image(systemName: "bed.double.fill")
+                            .foregroundColor(.white)
+                            .font(.title2)
+                        Text("Sleep | Wake Up")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                        Spacer()
+                    }
                 }
                 
                 Spacer()
+                // White line
+                Divider()
+                    .background(Color.white)
                 
                 HStack{
                     Text("7:30")
                         .font(.system(size: 64.0, weight: .thin,  design: .default))
                     Text("AM")
                         .font(.system(.largeTitle, design: .default, weight: .thin))
+                      
                     
                     Spacer()
                     
                     Toggle("",isOn: Binding.constant(false))
                 }
-                .padding()
+                Divider()
+                    .background(Color.white)
+                Spacer()
+                    
                 
                 
                 

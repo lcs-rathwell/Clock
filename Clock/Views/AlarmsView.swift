3c336c9e9a35958e936990066b3eed3bd2c1445e
Jack Rathwell <jrathwell@lcs.on.ca>
2024-12-18 11:25:08 -0500

    built Sleep Wake up view

--- Clock/Views/AlarmsView.swift
+++ Clock/Views/AlarmsView.swift
@@ -9,10 +9,60 @@ import SwiftUI
 
 struct AlarmsView: View {
     var body: some View {
-        Text("Alarms")
+        NavigationStack{
+            VStack{
+                HStack {
+                    Image(systemName: "bed.double.fill")
+                        .foregroundColor(.white)
+                        .font(.title2)
+                    Text("Sleep | Wake Up")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    
+                }
+                
+                Spacer()
+                
+                HStack{
+                    Text("7:30")
+                        .font(.system(size: 64.0, weight: .thin,  design: .default))
+                    Text("AM")
+                        .font(.system(.largeTitle, design: .default, weight: .thin))
+                    
+                    Spacer()
+                    
+                    Toggle("",isOn: Binding.constant(false))
+                }
+                .padding()
+                
+                
+                
+            }
+            .navigationTitle("Alarms")
+            .toolbar {
+                        ToolbarItem(placement: .topBarLeading) {
+      
+                    Button("Edit") {
+                        // Does nothing right now
+                    }
+                }
+               
+                    
+                
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "plus")
+                        
+                    }
+                }
+            }
+        }
     }
 }
 
+
 #Preview {
-    AlarmsView()
+    LandingView()
 }
